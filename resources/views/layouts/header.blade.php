e2c84accf26133509b8fc1128665984056ace1f8
Projet_Dev Dev <projet_dev@laloyalevie.local>
2025-06-26 10:56:57 +0000

    update header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,17 +11,18 @@
                                     height="33"></a>
                             </div>
                     </div>
+                    <center class="d-none d-lg-block">
+                        <div class="flat-bt-top">
+                            <a class="tf-btn primary" href="{{ route('reservation') }}">Faire une reservation</a>
+                        </div>
+                    </center>
                     <div class="header-account">
                         <div class="register">
                             <ul class="d-flex">
                                 <li><a href="#modalLogin" data-bs-toggle="modal">Connexion</a></li>
-                                <li>/</li>
-                                <li><a href="#modalRegister" data-bs-toggle="modal">S'inscrire</a></li>
                             </ul>
                         </div>
-                        <div class="flat-bt-top">
-                            <a class="tf-btn primary" href="{{ route('reservation') }}">Faire une reservation</a>
-                        </div>
+                        
                     </div>
 
                     <div class="mobile-nav-toggler mobile-button"><span></span></div>
@@ -37,13 +38,11 @@
     <div class="mobile-menu">
         <div class="menu-backdrop"></div>
         <nav class="menu-box">
-            <div class="nav-logo"><a href="index.html"><img src="images/logo/logo@2x.png" alt="nav-logo" width="174"
+            <div class="nav-logo"><a href="index.html"><img src="{{ asset('assets/images/logo/logo@2x.png')}}" alt="nav-logo" width="174"
                         height="44"></a></div>
             <div class="bottom-canvas">
                 <div class="login-box flex align-items-center">
                     <a href="#modalLogin" data-bs-toggle="modal">Connexion</a>
-                    <span>/</span>
-                    <a href="#modalRegister" data-bs-toggle="modal">Inscription</a>
                 </div>
                 <div class="menu-outer"></div>
                 <div class="button-mobi-sell">
@@ -64,56 +63,4 @@
     </div>
     <!-- End Mobile Menu -->
 
-    <!-- popup login -->
-    <div class="modal fade" id="modalLogin">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="flat-account bg-surface">
-                    <h3 class="title text-center">Connexion</h3>
-                    <span class="close-modal icon-close2" data-bs-dismiss="modal"></span>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <fieldset class="box-fieldset">
-                            <label for="email">Votre adresse email<span>*</span>:</label> 
-                            <input id="email" type="email" class="form-contact style-1 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </fieldset>
-                        <fieldset class="box-fieldset">
-                            <label for="pass">Votre mot de passe<span>*</span>:</label>
-                            <div class="box-password">
-                                <input type="password" class="form-contact style-1 password-field @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
-                                <span class="show-pass">
-                                    <i class="icon-pass icon-eye"></i>
-                                    <i class="icon-pass icon-eye-off"></i>
-                                </span>
-                            </div>
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </fieldset>
-                        <div class="d-flex justify-content-between flex-wrap gap-12">
-                            <fieldset class="d-flex align-items-center gap-6">
-                                <input type="checkbox" class="tf-checkbox style-2" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label for="remember" class="caption-1 text-variant-1">Se souvenir de moi</label>
-                            </fieldset>
-                            {{-- @if (Route::has('password.request'))
-                                <a href="{{ route('password.request') }}" class="caption-1 text-primary">Mot de passe oublie ?</a>
-                            @endif --}}
-                        </div>
-                        <button type="submit" class="tf-btn primary w-100">Se connecter</button>
-                    </form>
-                </div>
-            </div> 
-        </div>
-    </div>
-
 </header>
-
-
