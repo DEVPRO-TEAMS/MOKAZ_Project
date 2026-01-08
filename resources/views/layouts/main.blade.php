71b29483ed1c921ff44a0a08d271a2a4f17e0894
bruceyapo <bruceyapo2000@gmail.com>
2026-01-08 10:42:18 +0000

    Amelioration search

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
 
 <head>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NNNZRNVL');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <title>MOKAZ - Plateforme Immobilier</title>
 
@@ -157,6 +165,10 @@
 </head>
 
 <body class="body counter-scroll">
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNNZRNVL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     @php
         $reservations = App\Models\Reservation::all();
